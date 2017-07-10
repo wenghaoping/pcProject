5adbf750781b9bf7ba244e14a0dc57f53f286eb6
youmm <youmm@tongdaohui.com>
2017-07-10 16:51:01 +0800

    阮千军
    1.照常开发

--- src/views/login/telephoneLogin.vue
+++ src/views/login/telephoneLogin.vue
@@ -5,7 +5,7 @@
       <input class="code" v-model="captcha" placeholder="请输入验证码">
       <el-button type="text" @click="getCode">获取验证码</el-button>
     </div>
-    <el-button class="loginBtn tc">登录/注册</el-button>
+    <el-button class="loginBtn tc" @click="login">登录/注册</el-button>
   </div>
 </template>
 
@@ -15,20 +15,39 @@
     data () {
       return {
         telephone: '',
-        captcha: ''
+        captcha: '',
+        is_exist:NaN,
       }
     },
     methods: {
+//    获取验证码
       getCode(){
-        if (this.telephone) {
+        var myreg = /^(1+\d{10})|(159+\d{8})|(153+\d{8})$/;
+        if (!myreg.test(this.telephone)) {
+          this.$notify.error({
+            title: '错误',
+            message: '请输入有效的手机号码',
+            offset:400,
+            duration:1000
+          });
+        } else {
           this.$http.post(this.URL.authCaptcha, {
             user_mobile: this.telephone
           }).then(res => {
-            console.log(res)
+            console.log('验证码发送成功')
           })
-        } else {
-
         }
+      },
+//    注册或者登录
+      login(){
+//         is_exist: 0:新用户;1:老用户;NaN:没有请求过验证码
+          if(this.is_exist==0){
+//            this.$http.post(this.URL.)
+          }else if(this.is_exist==1){
+
+          }else{
+
+          }
       },
     }
   }
