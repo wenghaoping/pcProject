eab7efccba0bc79d8f72a24b9869fd662e4a14c3
youmm <youmm@tongdaohui.com>
2017-08-11 18:56:49 +0800

    2017.8.11 下班前 阮
    1.我的项目列表页面的项目推送弹框头部的padding-left调整
    2.将推送项目的头部文本由项目改为了项目介绍

--- src/views/workBench/myProject/projectPush2.vue
+++ src/views/workBench/myProject/projectPush2.vue
@@ -314,10 +314,10 @@
   import customerAddContacts from '../../../components/customerAddContacts.vue'
   import { mapState } from 'vuex'
   export default {
-  props: ["dialogPush",'proid','proName','emitPush'],
+  props: ["dialogPush",'proid','proIntro','emitPush'],
   data () {
     return {
-      project_name:this.proName,
+      project_name:this.proIntro,
       project_id:this.proid,
       close:false,//默认关闭
       loading: false,
@@ -765,7 +765,7 @@
     //打开该弹框时
     dialogPush:function(e){
       if(e===true){
-        this.project_name=this.proName;
+        this.project_name=this.proIntro;
         this.project_id=this.proid;
         this.initData();
         //为了重置checkBox状态
